53ae898fec9db63422ff7d736a60f454effffdfc
Noah Kantrowitz <noah@coderanger.net>
2017-04-30 23:51:43 -0700

    Checkpoint.

--- generators/project/index.js
+++ generators/project/index.js
@@ -20,6 +20,18 @@ const fs = require('fs');
 const Base = require('../base');
 
 module.exports = Base.extend({
+  initializing: function() {
+    if(!this.config.get('created')) {
+      // Check if it was actually already created outside of yo.
+      var remotes = this.spawnCommandSync('git', ['remote', '-v'], {stdio: ['ignore', 'pipe', 'ignore']}).stdout.toString();
+      if(/github\.com/.test(remotes)) {
+        // It was created.
+        this.config.set('created', true);
+      }
+    }
+    // Track if created was set before prompting.
+    this.wasCreated = this.config.get('created');
+  },
   prompting: function () {
     var prompts = [];
 
@@ -28,8 +40,14 @@ module.exports = Base.extend({
       prompts.push({
         type: 'input',
         name: 'name',
-        message: 'Your cookbook name',
+        message: 'Your gem name',
         default: this.appname.replace(/\s+/g, '-'),
+      });
+      prompts.push({
+        type: 'input',
+        name: 'cookbookName',
+        message: 'Your cookbook name',
+        default: 'auto',
       });
     }
 
@@ -45,24 +63,28 @@ module.exports = Base.extend({
 
     return this.prompt(prompts).then(function (answers) {
       if(answers.name) this.config.set('name', answers.name);
+      if(answers.cookbookName) this.config.set('cookbookName', answers.cookbookName);
       if(answers.created) this.config.set('created', answers.created);
     }.bind(this));
   },
   writing: function () {
     if(!this._folderExists('.git')) this.spawnCommandSync('git', ['init']);
-    if(this.config.get('created')) {
-      var remotes = this.spawnCommandSync('git', ['remote', '-v'], {stdio: ['ignore', 'pipe', 'ignore']}).stdout.toString();
-      if(!/github\.com/.test(remotes)) {
-        // Create the GitHub repo and enable Travis-CI.
-      }
+    if(this.config.get('created') && !this.wasCreated) {
+      // Create the GitHub repo and enable Travis-CI.
+      this.spawnCommandSync('git', ['create', 'poise/'+this.config.get('name')]);
+      this.spawnCommandSync('travis', ['enable', '--no-interactive']);
     }
 
     var copyTpl = function(path, context) {
       context = context || {};
-      context.cookbookName = context.cookbookName || this.config.get('name');
-      context.displayName = context.displayName || context.cookbookName.replace(/(_|\b)(\w+)/g, function(match, p1, p2) { return p1 + p2[0].toLocaleUpperCase() + p2.substr(1) });
-      context.requireName = context.requireName || context.cookbookName.replace(/-/g, '_');
-      context.moduleName = context.moduleName || context.cookbookName.replace(/(-|_|\b)([^_-]+)/g, function(match, p1, p2) { return p2[0].toLocaleUpperCase() + p2.substr(1) });
+      context.gemName = context.requireName || this.config.get('name');
+      context.cookbookName = context.cookbookName || this.config.get('cookbookName');
+      if(context.cookbookName == 'auto') {
+        context.cookbookName = context.gemName.replace(/(^(chef|cookbook)[_-])|([_-](chef|cookbook))$/g, '');
+      }
+      context.requireName = context.requireName || context.gemName.replace(/-/g, '_');
+      context.displayName = context.displayName || context.gemName.replace(/(_|\b)(\w+)/g, function(match, p1, p2) { return p1 + p2[0].toLocaleUpperCase() + p2.substr(1) });
+      context.moduleName = context.moduleName || context.gemName.replace(/(-|_|\b)([^_-]+)/g, function(match, p1, p2) { return p2[0].toLocaleUpperCase() + p2.substr(1) });
       context.pathName = context.pathName || context.requireName;
       this.copyTpl(path, context);
     }.bind(this);
@@ -88,22 +110,22 @@ module.exports = Base.extend({
     copyTpl('Gemfile', {gemfileUser: gemfileUser});
     copyTpl('LICENSE');
     copyTpl('Rakefile');
+    // Need to specify the filename because it doesn't follow the usual pattern,
+    // uses the gemName not the requireName.
     copyTplOnce('app.gemspec', {out: this.config.get('name')+'.gemspec'});
     copyTplOnce('README.md');
-    if(!this._folderExists('chef')) {
+    if(!this._folderExists('lib')) {
       copyTplOnce('chef/attributes/default.rb');
       copyTplOnce('chef/recipes/default.rb');
+      copyTpl('lib/app.rb');
+      copyTpl('lib/app/cheftie.rb');
+      copyTpl('lib/app/resources.rb');
+      copyTpl('lib/app/version.rb');
+      copyTpl('lib/app/resources/app.rb');
     }
-    if(!this._folderExists('lib')) {
-      copyTplOnce('lib/app.rb');
-      copyTplOnce('lib/app/cheftie.rb');
-      copyTplOnce('lib/app/resources.rb');
-      copyTplOnce('lib/app/version.rb');
-      copyTplOnce('lib/app/resources/app.rb');
-    }
-    copyTpl('test/cookbook/metadata.rb')
     if(!this._folderExists('test')) {
-      copyTplOnce('test/cookbook/recipes/default.rb')
+      copyTpl('test/cookbook/metadata.rb')
+      copyTpl('test/cookbook/recipes/default.rb')
     }
     copyTplOnce('test/spec/spec_helper.rb')
   },
